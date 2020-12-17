c939c4581b09c2eeb9d1a5b6476bd868155507ce
itsccn <chen.sun@itsccn.com>
2020-12-17 18:06:41 +0800

    update:添加强制推送参数

--- backup.sh
+++ backup.sh
@@ -3,5 +3,5 @@ nowDate=$(date "+%Y-%m-%d %H:%M:%S")
 dir=$PWD
 docker exec db  sh -c 'exec mysqldump   -uroot -p"$MYSQL_ROOT_PASSWORD" wordpress' > $dir/mysql/db.sql
 
-cd $dir && git -c core.quotepath=false -c log.showSignature=false fetch origin --recurse-submodules=no --progress --prune  && git add -A  && git commit -m "auto backup $nowDate" && git -c core.quotepath=false -c log.showSignature=false push --progress --porcelain origin main
+cd $dir && git -c core.quotepath=false -c log.showSignature=false fetch origin --recurse-submodules=no --progress --prune  && git add -A  && git commit -m "auto backup $nowDate" && git -c core.quotepath=false -c log.showSignature=false push -f  --progress --porcelain origin main
 
